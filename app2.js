590a65f086bbde933b7560e0949501b236070797
Stefan Blandin <stefanblandin@yahoo.com>
2017-11-03 22:40:23 -0700

    UI/datavis
    This update pushes out a much-needed UI and data visualization function.
    And now your results will be stored in your browser! So if you lose the
    page you can revert your network back to the last version. To delete
    this, click "Init network".

--- app2.js
+++ app2.js
@@ -4,23 +4,45 @@ var add = require('vectors/add-nd');
 var fs = require('fs');
 
 var values = {};
-var weightsize = 2;
-var biassize = 2;
+var weightsize = 10;
+var biassize = 10;
 
+var canvas = {}
+var context = {}
+
+var runlist = [];
+var imagelist = [];
 
 var main=function(){
+
+	preread();
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");
+
 	//get a random image from the database.
 	values = dataread(Math.floor(Math.random()*60000));
 
+	$('.Plzno').click(function(event) {
+		localStorage.removeItem("Handwriting_NN_lastrun")
+		network.init();
+		network.generateInputs();
+		network.run();
+	});
+
 	$('.gobutton').click(function(event) {
 		//for the github version let's just do this once. And never change variables.
-		for (var i=0; i<800; i++)
+		var runcount=16;
+		for (var i=0; i<runcount; i++)
 		{
 			//get a random image from the database.
+			console.log("starting dataread");
 			values = dataread(Math.floor(Math.random()*60000));
-			mainPAUSE();
+			console.log("ending dataread");
+			runEpoch();
 
-			console.log("Testing #:"+values.label+", Global progress: "+((Math.round(((i*100)/800))*100)/100)+"%")
+			console.log("Testing #:"+values.label+", Global progress: "+((Math.round(((i*100)/runcount))*100)/100)+"%");
+			drawCanvas();
 		}
 	});
 
@@ -29,15 +51,34 @@ var main=function(){
 		for (var i=0; i<1; i++)
 		{
 			//get a random image from the database.
+			console.log("starting dataread");
 			values = dataread(Math.floor(Math.random()*60000));
+			console.log("ending dataread");
+			runEpoch();
+
 			console.log("Testing #:"+values.label);
-			mainPAUSE();
+			drawCanvas();
 		}
 	});
 
 	network.init(sizes);
 	network.generateInputs();
+
+	//retreive last run and restore it.
+	if (localStorage.getItem("Handwriting_NN_lastrun")!==null)
+	{
+		var cor=JSON.parse(localStorage.getItem("Handwriting_NN_lastrun"))
+		network.pushCoordsToNetwork(cor.coords)
+		console.log(localStorage.getItem("Handwriting_NN_lastrun"))
+	}
+	else
+	{
+		alert("this looks like your first run.")
+	}
+
 	network.run();
+	runlist.push({cost:network.getCost(),label:values.label})
+	drawCanvas();
 	$(".run1here").text(network.getCost())
 }
 //sizes is an array of the neural network layer sizes. The first must equal the number of inputs, for example mine is 784 because each image is 
@@ -47,20 +88,61 @@ var main=function(){
 
 var sizes = [784,20,10];
 
-var mainPAUSE=function()
+var runEpoch=function()
 {
-	
-
 	console.clear();
 	//now the climber has put his foot in a random place on the terrain. We need to store the height of his foot. 
+	console.log("starting store");
 	network.store();
+	console.log("Store finished")
 	//next, the climber must put his other foot around the terrain near his current step, and feel around until he has a better spot.
+
+	var origcoords = network.historicalweightbias[network.historicalweightbias.length-1].coords;
+	console.log("starting makenewbiases");
 	var delts=network.makeNewBiases();
 	//ok so we assume he knows the direction to put his next foot.
-	// 
-	network.applydelts(delts,1);
+	var line_samples=[];
+	var maxdis=10;
+	var curdis=0;
+
+	//ok so originally, I would have been satisfied at "delts" and applied them to the network. but unforuntately
+	//delts is pretty much an arbitrary distance (the direction is right, but I digress)
+	//so what we'll need to do is do a straight-line minimization step.
+
+	//so now imagine the climber has walked in a straight line for an arbitrary (and long) distance, specified by maxdis.
+	//he now remembers the lowest point, and that's his next step.
+	for (var i=0; i<100; i++)
+	{
+		network.pushCoordsToNetwork(origcoords)
+		network.applydelts(delts,i/(100/maxdis));
+		network.run();
+		var c = network.getCost()
+		line_samples.push({scalar:i/(100/maxdis),cost:c})
+		console.log("Testing scalar "+i/(100/maxdis)+", cost was: "+c)
+	}
+
+	//now we have a list of points on this line. Let's find the minimum and go from there.
+	var curmin=1000000000;
+	var minid=0;
+	for (var i=0; i<line_samples.length; i++)
+	{
+		if (line_samples[i].cost<curmin)
+		{
+			curmin=line_samples[i].cost;
+			minid=i;
+		}
+	}
+	console.log("Best scalar found at "+line_samples[minid].scalar+" With value "+line_samples[minid].cost)
+
+	//now we have the lowest point on that line. That'll be our next start.
+	network.pushCoordsToNetwork(origcoords);
+	network.applydelts(delts,line_samples[minid].scalar)
 	network.run();
+	//we should now have the minimum point for this line. or at least a much larger line than it was. 
+
 	$(".run2here").text(network.getCost())
+	runlist.push({cost:network.getCost(),label:values.label})
+	
 }
 
 
@@ -133,21 +215,10 @@ var network=
 			{
 				outstr+="<p>"+this.neurons[i][ii].result+"</p>";
 			}
-			if (i==1)
-			$('.hd1').html(outstr);
-
-			if (i==2)
-			$('.hd2').html(outstr);
-
-			if (i==3)
-			$('.hd3').html(outstr);
 
 			if (i==this.neurons.length-1)
 			$('.hd4').html(outstr);
 		}
-
-		$('.actualnumberhere').text(values.label);
-
 	},
 	getCost:function(){
 		var opts=[];
@@ -171,7 +242,9 @@ var network=
 		r.cost=this.getCost();
 
 		//now we need to encode the values that got us here. 
-		r.coords=[]
+		r.coords=[];
+
+		r.value=values.label;
 		//really, this means that this is a spot in an n-dimensional coordinate plane. I know that sounds stupid. But what we're doing is making
 		//a hill-climbing algorithm but instead of a 2D terrain with a 3rd dimension representing height, it's a bajillion dimension thing with a bazzilion+1'th dimension representing cost.
 		//we're going to make a machine that can find its way around this "Hill" using the same techniques.
@@ -195,6 +268,9 @@ var network=
 		//we now have the "Foot position" of this climber. 
 		//to avoid using "bazillion" we're calling the current r.coords.length "Z". It should number in the hundreds of thousands. 
 		//actual calculations show that it's 132,600 (Or something around there) for this NN. 
+
+		//and now to ensure we don't lose all of our progress at every restart, the arguments will now be stored in localstorage.
+		localStorage.setItem("Handwriting_NN_lastrun",JSON.stringify(r))
 	},
 	makeNewBiases:function(){
 		//delt[coordval] represents our best guess of the correct direction to go with that particular variable. 
@@ -212,6 +288,9 @@ var network=
 
 			// how much we changed this value
 			var deltaval=(Math.random()*weightsize/2)-(weightsize/4);
+
+			if (Math.abs(deltaval)<1)
+			deltaval = Math.sign(deltaval);
 
 			newcoords[i]+=deltaval;
 			//now we need to feed it back into the network
@@ -265,9 +344,9 @@ var network=
 				//ok, we're at "each neuron" level.
 				for (var iii=0; iii<this.neurons[i][ii].inputs.length; iii++)
 				{
-					this.neurons[i][ii].inputs[iii].weight+=delts[v]/lsl;
+					this.neurons[i][ii].inputs[iii].weight+=delts[v]*lsl;
 					v+=1;
-					this.neurons[i][ii].inputs[iii].bias+=delts[v]/lsl;
+					this.neurons[i][ii].inputs[iii].bias+=delts[v]*lsl;
 					v+=1;
 				}
 			}
@@ -340,24 +419,35 @@ function newneuron_input(xy){
 	return n;
 }
 
-function dataread(image)
-{
-	//this reads the NIST data. If you're using my raw code, do this however you like. 
+function preread(){
+		//this reads the NIST data. If you're using my raw code, do this however you like. 
 	var dataFileBuffer  = fs.readFileSync(__dirname + '/train-images-idx3-ubyte/train-images.idx3-ubyte');
 	var labelFileBuffer = fs.readFileSync(__dirname + '/train-labels-idx1-ubyte/train-labels.idx1-ubyte');
-	var pixelValues     = [];
+	
+
+	for (var image = 0; image<59900; image++)
+	{
+		var pixelValues     = [];
  
-	var pixels = [];
-
-	for (var x = 0; x <= 27; x++) {
-		for (var y = 0; y <= 27; y++) {
-			pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 15]);
+		var pixels = [];
+		for (var x = 0; x <= 27; x++) {
+			for (var y = 0; y <= 27; y++) {
+				pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 15]);
+			}
 		}
-	}
 
-    var imageData  = {};
+	var imageData  = {};
     imageData.label 	= JSON.stringify(labelFileBuffer[image + 8])
     imageData.pixels 	= pixels;
+
+    imagelist.push(imageData);
+	}
+
+}
+
+function dataread(image)
+{
+	var imageData = imagelist[image];
 
 	return imageData;
 }
@@ -429,7 +519,7 @@ function successvector(label){
 
 	if (label==8)
 	{
-		r=[0,0,0,0,1,0,0,0,1,0]
+		r=[0,0,0,0,0,0,0,0,1,0]
 	}
 
 	if (label==9)
@@ -440,5 +530,63 @@ function successvector(label){
 	return r;
 }
 
+function drawCanvas(){
+
+	context.clearRect(0,0,canvas.width,canvas.height);
+
+	var steps = (canvas.width-60)/runlist.length;
+
+	//draw horizontal axis
+	context.beginPath();
+	context.moveTo(30,canvas.height-60)
+	context.lineTo(canvas.width-30,canvas.height-60);
+	context.strokeStyle="black";
+	context.lineWidth=1;
+	context.lineJoin="round";
+	context.stroke();
+
+	context.fillText("Horizontal axis: Epoches (major runs). The numbers indicate what digit being tested. Vertical axis: Cost. Lower is better.",30,canvas.height-15)
+
+
+	if (runlist.length>0)
+	{
+		//make axis points
+		for (var i in runlist)
+		{
+			//only draw certain labels. Don't want to get it too cluttered.
+			if ((runlist.length<15) || (runlist.length<30 && runlist.length>15 && ((i%4)==1)) || (runlist.length<600 && runlist.length>30 && ((i%8)==1))|| i==(runlist.length-1))
+			{
+				context.beginPath();
+				context.strokeStyle="gray";
+	
+				context.moveTo(30+(i*steps),canvas.height-45)
+				context.lineTo(30+(i*steps),(canvas.height-45)-(runlist[i].cost*(540/7)))
+				context.fillText(runlist[i].label,30+(i*steps),canvas.height-30)
+				context.fillText("Cost: "+(Math.round(runlist[i].cost*1000)/1000),30+(i*(steps)),(canvas.height-60)-(runlist[i].cost*(540/7))-15)
+				context.stroke();
+			}
+			else
+			{
+				//however zero cost points are always significant, so always label them
+				if (runlist[i].cost<.01)
+				{
+					context.fillText(runlist[i].label,30+(i*steps),canvas.height-30)
+				}
+			}
+		}
+	
+		context.beginPath()
+		
+		context.moveTo(30,(canvas.height-60)-(runlist[0].cost*(540/7)))
+		for (var i in runlist)
+		{
+			context.lineTo(30+(i*(steps)),(canvas.height-60)-(runlist[i].cost*(540/7)))
+		}
+		context.lineWidth=4;
+		context.lineCap="round";
+		context.strokeStyle="blue";
+		context.stroke();
+	}
+}
 
 $(document).ready(main)
